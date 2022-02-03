14304a7dc5286326b2408142f6497d8a00c9af7f
ashish <ajaintester@gmaiil.com>
2022-02-03 05:38:48 +0000

    Updated script

--- script/update_backend.sh
+++ script/update_backend.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 cd
 cd ~/fullstack_tutorial
 git pull
 cd backend
 kill -9 `ps -eaf | grep backend/main.py | grep -v grep | awk '{print $2}'`
+pwd
 rm nohup.out
 deactivate
 python3 -m venv python_modules
